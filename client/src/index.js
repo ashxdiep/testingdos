4af916a1a8f7d3b3254a5bb8409c65288ba23153
ash.d.321@gmail.com <ash.d.321@gmail.com>
2018-04-12 17:17:39 -0500

    voting

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import { AppContainer } from 'react-hot-loader';
+import Voting from './components/Voting';
 import registerServiceWorker from './registerServiceWorker';
 
 
@@ -10,7 +10,7 @@ const rootEl = document.getElementById('root');
 
 ReactDOM.render(
   <AppContainer>
-    <App />
+    <Voting pair = { pair }
   </AppContainer>, rootEl
 );
 
