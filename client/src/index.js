efa35991fd6bde3a30d69c09119f4e9fbf333356
ash.d.321@gmail.com <ash.d.321@gmail.com>
2018-04-12 16:02:03 -0500

    react loader

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { AppContainer } from 'react-hot-loader';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const rootEl = document.getElementById('root');
+
+ReactDOM.render(
+  <AppContainer>
+    <App />
+  </AppContainer>, rootEl
+);
+
+if (module.hot) {
+    module.hot.accept('./App', () => {
+        const NextApp = require('./App').default; // eslint-disable-line global-require
+        ReactDOM.render(
+            <AppContainer>
+                <NextApp />
+            </AppContainer>,
+            rootEl
+        );
+    });
+}
 registerServiceWorker();
